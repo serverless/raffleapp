a7e51b5711a308409a080112f5c13cde5d82cb14
Rupak Ganguly <rupakg@gmail.com>
2017-09-29 18:33:02 +0530

    Add input data validation.

--- services/raffle/handlers/create.js
+++ services/raffle/handlers/create.js
@@ -5,10 +5,25 @@ const documentClient = new awsSDK.DynamoDB.DocumentClient();
 const RAFFLE_TABLE = process.env.RAFFLE_TABLE;
 
 module.exports.handler = (event, context, callback) => {
-  const body = JSON.parse(event.body);
-  const name = body.name;
-  const admins = body.admins ? body.admins : [];
+
+  console.log(JSON.stringify(event));
+  
+  const body = event.body ? JSON.parse(event.body) : null;
+  const name = body ? body.name : '';
+  const admins = body && body.admins ? body.admins : [];
   const timestamp = new Date().getTime();
+
+  // input validation
+  if (!body || !name || !admins) {
+    const err = {
+      statusCode: 422,
+      body: JSON.stringify({
+        message: "Bad input data.",
+        input: event.body,
+      }),
+    };
+    return callback(null, err);
+  }
 
   const shortcode = generateShortCode();
   const params = {
