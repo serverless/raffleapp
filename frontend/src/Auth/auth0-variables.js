cd76f40cf62269fcba7a97f1e82da7ec102487be
davidwells <david@inboundnow.com>
2017-10-06 15:58:04 -0700

    update variables for netlify context

--- frontend/src/Auth/auth0-variables.js
+++ frontend/src/Auth/auth0-variables.js
@@ -1,5 +1,12 @@
+
+let callbackUrl = 'http://localhost:3000/callback'
+if(process.env && process.env.REPOSITORY_URL) {
+  // in netlify context. See http://bit.ly/2y86cil
+  callbackUrl = 'https://serverless-raffle.netlify.com/callback'
+}
+
 export const AUTH_CONFIG = {
   domain: 'serverlessinc.auth0.com',
   clientId: '37p4vtkwmDGQwwG1FYhwOGiu3OjZoHo5',
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: callbackUrl
 }
